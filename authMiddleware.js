3d09b526efd5e0b6424abf38ddff03667a940b04
Akshatt02 <123cs0073@iiitk.ac.in>
2026-03-13 08:22:24 +0530

    a

--- authMiddleware.js
+++
@@ -1,9 +0,0 @@
-const jwt = require("jsonwebtoken")
-
-module.exports = function (req, res, next) {
-    const token = req.headers["authorization"]
-    if (!token) return res.status(401).json({ msg: "No token" })
-    const decoded = jwt.verify(token, "secretkey")
-    req.user = decoded
-    next()
-}
